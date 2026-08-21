06402f02251f1854521f7107a69307b3cd24f99c
Dinesh <124895635+Dinesh1441@users.noreply.github.com>
2026-08-21 17:34:17 +0530

    updated

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -163,7 +163,7 @@ const ProductPage = () => {
 
   const shareData = {
     title: product.name,
-    text: `${product.name}\nMRP: ₹${product.mrp}\nSelling Price: ₹${product.price}\n${productUrl}`,
+    text: `${product.name}\nPrice: ₹${product.price}\n${productUrl}`,
     url: productUrl,
   };
 
@@ -173,7 +173,7 @@ const ProductPage = () => {
     } else {
       // Fallback: copy product details + link
       await navigator.clipboard.writeText(
-        `${product.name}\nPrice: ₹${product.sellingPrice}\n${productUrl}`
+        `${product.name}\nPrice: ₹${product.price}\n${productUrl}`
       );
 
       alert("Product details copied!");
@@ -539,7 +539,7 @@ const ProductPage = () => {
             </div>
             <button 
               onClick={handleBuyNow}
-              className="bg-black hover:bg-black-500 w-full md:w-auto justify-center  hover:bg-orange-600 text-white px-4 py-2.5 rounded-lg font-semibold text-sm transition-all flex items-center gap-2"
+              className="bg-black hover:bg-black-500 w-full md:w-auto justify-center text-white px-4 py-2.5 rounded-lg font-semibold text-sm transition-all flex items-center gap-2"
             >
                <div className='flex'>
                   <div className='flex w-5 h-5  ml-[-7px] items-center justify-center rounded-full bg-white' >
