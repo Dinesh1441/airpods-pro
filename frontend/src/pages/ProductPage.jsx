cab422675530c77dce2811fea88826deca4237c7
Dinesh <124895635+Dinesh1441@users.noreply.github.com>
2026-08-21 09:46:36 +0530

    image updated

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -17,11 +17,11 @@ import pay1 from '../assets/images/payment/googlepay.svg';
 import pay2 from '../assets/images/payment/paytm.svg';
 import pay3 from '../assets/images/payment/phonepe.svg';
 
-import img1 from '../assets/images/product/1.png';
-import img2 from '../assets/images/product/2.png';
-import img3 from '../assets/images/product/3.png';
-import img4 from '../assets/images/product/4.png';
-import img5 from '../assets/images/product/5.png';
+import img1 from '../assets/images/product/1.webp';
+import img2 from '../assets/images/product/2.webp';
+import img3 from '../assets/images/product/3.webp';
+import img4 from '../assets/images/product/4.webp';
+import img5 from '../assets/images/product/5.webp';
 
 
 import video1 from '../assets/images/video/1.mp4';
@@ -29,7 +29,7 @@ import video2 from '../assets/images/video/2.mp4';
 import video3 from '../assets/images/video/3.mp4';
 import video4 from '../assets/images/video/4.mp4';
 
-import batchs from '../assets/images/batchs/1.png';
+import batchs from '../assets/images/batchs/1.webp';
 import banner from '../assets/images/banner/1.png';
 
 import review1 from '../assets/images/reviews/1.png';
